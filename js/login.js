48c4067cae17332a48bfce8b00a7ccb87f91c4b0
Arifu Islam Adil <adil.islam.arif@gmail.com>
2021-08-20 12:42:10 +0600

    added js login

---
+++ js/login.js
@@ -0,0 +1,19 @@
+//use event listener at the submit button
+document.getElementById('login-submit').addEventListener('click', function
+    () {
+    //get user value by using value
+    const emailField = document.getElementById('user-email')
+    const userEmail = emailField.value;
+
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    //Link to another page
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        window.location.href = "banking.html"
+    }
+
+});
+
+
